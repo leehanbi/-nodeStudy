78d2fddb2b7e00f641af3d692e596a4e0d373d18
leehanbi <dlgksql05@gmail.com>
2019-01-20 19:45:02 +0900

    node.js post 방식 커밋 노드 포스트 방식을 통해 데이터를 얻는 방법 스터디 .. 커밋

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring')
 
 function templatHTML(tatle,list,body){
   return `
@@ -13,6 +14,7 @@ function templatHTML(tatle,list,body){
   <body>
   <h1><a href="/">Web</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
     <p>
     </p>
@@ -61,7 +63,43 @@ var app = http.createServer(function(request,response){
           });
       });
     }
-    }else{
+  } else if (pathname === '/create') {
+    fs.readdir('./data',function( error,filelist){
+      var templat = makeInfo('Web - create',filelist,`
+      <form action="http://localhost:3000/create_process" method="post">
+        <p><input type="text" name ="tatle" placeholder="tatle"></p>
+        <p><textarea name="description" rows="8" cols="80" name ="description"
+        placeholder="description"> </textarea></p>
+        <p><input type="submit"></p>
+      </form>
+       `);
+      response.writeHead(200);
+      response.end(templat);
+    })
+  }else if(pathname === '/create_process') {
+    var body = '';
+
+    // requst data를 통해서 데이터가 들어와 body에 저장
+    request.on('data',function(data){
+        body += data;
+    });
+
+    // 그 후 end`로 넘어와 post에 담아서 뽑아 쓸수 있다
+    request.on('end',function(){
+      // post에 body데이터가 저장됨
+      var post = qs.parse(body);
+      var tatle = post.tatle;
+      var description = post.description;
+
+      console.log(post);
+      console.log(post.tatle);
+      console.log(post.description);
+    });
+
+
+    response.writeHead(200);
+    response.end('success');
+  }else{
       response.writeHead(404);
       response.end('not found');
     }
