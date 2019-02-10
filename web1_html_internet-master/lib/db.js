6373a577218f09638e2c6c30ec47a6d3f082f4dd
이한비 <>
2019-02-10 13:42:19 +0900

    node Study commit

--- web1_html_internet-master/lib/db.js
+++ web1_html_internet-master/lib/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '12345678',
-    database : 'opentutorials'
+    host     : '',
+    user     : '',
+    password : '',
+    database : ''
   });
   db.connect();
 
